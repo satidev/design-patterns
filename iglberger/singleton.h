e2d5f6f24128d2f0095a43ef766b63f5805357da
abdurahm <shiras.abdurahman@ovgu.de>
2024-04-22 17:33:55 +0200

    Refactor singleton

--- iglberger/singleton.h
+++ iglberger/singleton.h
@@ -1,28 +1,36 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+#include <cassert>
+
 namespace Iglberger
 {
 struct Singleton final
 {
+// Private constructor.
 private:
     Singleton(){}
+
 public:
+    // Access point to unique instance.
     static Singleton& getInstance(){
-        static Singleton inst{};
+        static Singleton inst{};// Thread-safe from C++11.
         return inst;
     }
 
+    // Delete all copy and move operations.
     Singleton(Singleton const &) = delete;
     Singleton& operator=(Singleton const &) = delete;
     Singleton(Singleton &&) = delete;
     Singleton& operator=(Singleton &&) = delete;
-
-
 };
 
-void singletonEx();
-
+void singletonEx()
+{
+    auto& inst1 = Singleton::getInstance();
+    auto& inst2 = Singleton::getInstance();
+    assert(&inst1 == &inst2);
+}
 }// Iglberger namespace.
 
 #endif //SINGLETON_H
