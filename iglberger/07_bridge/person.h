03c72f53d59bebafc42980a9dc62c977af6b6d56
abdurahm <shiras.abdurahman@ovgu.de>
2024-04-29 14:31:48 +0200

    Move to different files

--- iglberger/07_bridge/person.h
+++ iglberger/07_bridge/person.h
@@ -8,12 +8,16 @@ namespace Iglberger
 struct Person
 {
 private:
+    // Declared but not defined in the class hiding implementation details.
+    // Defined in source file.
     struct Impl;
     std::unique_ptr<Impl> const pimpl_;
 public:
     Person();
     ~Person();
 
+    // We need to define copy operations due to unique pointer member and its
+    // constness.
     Person(const Person &person);
     Person(Person &&person);
     Person &operator=(const Person &person);
